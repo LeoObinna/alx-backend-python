d18b692bcfb5d2f338eb15f105f3f249d5a0cbbb
LeoObinna <leoaniude20@gmail.com>
2024-07-05 03:21:07 +0000

    Write a type-annotated function to_str that takes a float n as argument and returns the string representation of the float.

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Task 2's module- Basic annotations - to string.
+'''
+
+
+def floor(a: float) -> int:
+    '''This computes the floor of a floating-point number.
+    '''
+    return int(a)
