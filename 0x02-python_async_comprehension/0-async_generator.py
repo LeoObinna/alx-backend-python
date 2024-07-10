e2684f533ee0493fbfade243c1bf873e3a719e9a
LeoObinna <leoaniude20@gmail.com>
2024-07-10 03:08:46 +0000

    measure_runtime should measure the total runtime and return it,you will notice that the total runtime is roughly 10 seconds

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,12 +3,12 @@
 '''
 import asyncio
 import random
-from typing import Generator
+from typing import AsyncGenerator
 
 
-async def async_generator() -> Generator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None, None]:
     '''This generates a sequence of 10 numbers.
     '''
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.random() * 10
+        yield random.random(0, 10)
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
-'''Task 2's module - Run time for four parallel comprehensions.
+'''Task 2's module -  Run time for four parallel comprehensions.
 '''
 import asyncio
 import time
 from importlib import import_module as using
 
 
-async_comprehension = using('1-async_comprehensive').async_comprehensive
+async_comprehension = using('1-async_comprehension').async_comprehension
 
 
-async def meaure_runtime() -> float:
-    '''This executes async_comprehensive 4 times and measures the
+async def measure_runtime() -> float:
+    '''This executes async_comprehension 4 times and measures the
     total execution time.
     '''
     start_time = time.time()
-    await asyncio.gather(*(async_comprehensive() for _ in range(4)))
-    return time.time() -
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    total_runtime = time.time() - start_time
+    return total_runtime
